a499bc59bbfa21c71c5bfed7aa9e8f4fac3f6f28
Etienne Leroy Dev <geti.leroy@gmail.com>
2023-03-15 13:23:28 +0100

    bdd

---
+++ back/assets/jame.js
@@ -0,0 +1,13 @@
+var NB_DICE_SIDE = 6;
+let SCORING_DICE_VALUE = [1, 5];
+let SCORING_MULTIPLIER = [100, 50];
+
+let THRESHOLD_BONUS = 3;
+let STD_BONUS_MULTIPLIER = 100;
+let ACE_BONUS_MULTIPLIER = 1000;
+
+let DEFAULT_DICES_NB = 6;
+
+function roll_dice_set(nb_dice_to_roll) {
+    
+  }
